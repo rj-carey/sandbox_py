2f6232751dd920dffca04706423357bd89aafdc2
rj-carey <carey.ruaidhri@gmail.com>
2025-06-13 11:33:54 +0100

    Implemented priority queue and related tests.

---
+++ tests/test_priority_queue.py
@@ -0,0 +1,54 @@
+import pytest
+from structures.queue import Queue
+
+def test_add_and_peek():
+    s = Queue()
+    s.add(1)
+    s.add(2)
+    assert s.peek() == 1  # First added is on top
+    assert not s.isEmpty()
+
+def test_get():
+    s = Queue()
+    s.add(1)
+    s.add(2)
+    assert s.get() == 1
+    assert s.get() == 2
+    assert s.isEmpty()
+
+def test_get_empty_raises():
+    s = Queue()
+    with pytest.raises(IndexError, match="Queue is empty."):
+        s.get()
+
+def test_peek_empty_raises():
+    s = Queue()
+    with pytest.raises(IndexError, match="Queue is empty."):
+        s.peek()
+
+def test_is_empty_true_and_false():
+    s = Queue()
+    assert s.isEmpty()
+    s.add(10)
+    assert not s.isEmpty()
+
+def test_str_and_repr():
+    s = Queue()
+    s.add(1)
+    s.add(2)
+    s.add(3)
+    expected = "[1, 2, 3, ...]"
+    assert s.__str__() == expected
+    assert repr(s) == expected
+
+def test_priority():
+    s = Queue()
+    s.add(3)
+    s.add(4)
+    s.add(0)
+    s.add(1)
+    s.add(5)
+    s.add(2)
+    s.add(3)
+    s.add(3)
+    assert s.__str__() == "[0, 1, 2, 3, 3, 3, 4, 5, ...]"
